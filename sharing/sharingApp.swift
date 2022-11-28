4979264dbe5622562e93617caf79b4cd21a907ff
Yu <723156735@qq.com>
2022-11-28 17:34:27 +0800

    Initial Commit

---
+++ sharing/sharingApp.swift
@@ -0,0 +1,17 @@
+//
+//  sharingApp.swift
+//  sharing
+//
+//  Created by ZHANGYU on 2022/11/28.
+//
+
+import SwiftUI
+
+@main
+struct sharingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
